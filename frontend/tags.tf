8e9ab6e67f0bfe5bc7a240f3fc8087e4596b3869
aninchat <67307402+aninchat@users.noreply.github.com>
2023-10-02 22:57:34 +0530

    Create tags.tf

---
+++ frontend/tags.tf
@@ -0,0 +1,13 @@
+locals {
+  hosts = toset([
+    "a100",
+    "storage",
+    "h100",
+    "headend"
+  ])
+}
+
+resource "apstra_tag" "host_tags" {
+    for_each    = local.hosts
+    name        = each.key
+}
